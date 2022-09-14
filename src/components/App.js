64dd46bbf9d56448cd25b707f5b7e0a652d9741e
danielware <waredanielb@gmail.com>
2022-09-14 16:47:31 -0700

    EOD WIP styling.
    Co-Authored-By: Joseph Jack <joseph.w.jack@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import '../App.css';
 import React from 'react';
 import PostControl from "./PostControl";
 import Header from './Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
-      <PostControl/>
+      <Header/>         
+      <PostControl/>     
     </React.Fragment>
   );
 }
 
-// npm install redux@4.2.0 react-redux@8.0.2
+
 
 export default App;
